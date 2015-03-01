2ff3216a1d5b190da2cdd8d106e92d6dd0015f10
acbldie <antoniocbeldie@gmail.com>
2015-03-01 08:35:40 -0600

    Update register.aspx.cs

--- register.aspx.cs
+++ register.aspx.cs
@@ -1,14 +1,33 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class Register : System.Web.UI.Page
+public partial class _Default : System.Web.UI.Page
 {
+    List<string> referralCodes = new List<string>();
+
     protected void Page_Load(object sender, EventArgs e)
     {
 
+        referralCodes.Add("1337");
+       
+
+    }
+
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        if (Page.IsValid)
+        {
+            Response.Redirect("~/ThankYou.aspx");
+        }
+
+    }
+
+    protected void vldCustomCode_ServerValidate(object source, ServerValidateEventArgs args)
+    {
+        args.IsValid = referralCodes.Contains(args.Value);
     }
 }
