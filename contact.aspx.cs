d15a87df63fdd2920fbd970bcc66a68987daa19d
kyletamayo <kyle.tamayo@gmail.com>
2015-03-02 10:49:27 -0600

    Revert "Update and rename contact.aspx.cs to contact.aspx.cs(abeldie)"
    This reverts commit 1c0612d2a94fc6719e7fbb8b333ff369c5b483c4.

--- contact.aspx.cs
+++ contact.aspx.cs
@@ -1,20 +1,14 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class _contact : System.Web.UI.Page
+public partial class Contact : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
-    }
-
-    protected void collect_Click(object sender, EventArgs e)
-    {
-        
-            
     }
 }
