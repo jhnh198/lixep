becec8165759284657936e688879759703f3a7a8
John Hewitt <jchewitt@neiu.edu>
2015-04-03 15:48:49 -0500

    My Account , Games
    Gave my account some controls. Working a bit with games to have
    functionality

--- Secured_User_Pages/myAccount.aspx.cs
+++ Secured_User_Pages/myAccount.aspx.cs
@@ -9,6 +9,11 @@ public partial class Secured_User_Pages_myAccount : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        if (System.Web.HttpContext.Current.User.Identity.IsAuthenticated)
+        {
+            lblUserName.Text = System.Web.HttpContext.Current.User.Identity.Name;
+        }
+        else lblUserName.Text = "user not logged in or invald credentials.";
     }
+
 }
