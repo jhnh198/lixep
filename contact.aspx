3114ab14d3cb24e6b3da2cb86a4aec31d15c5bf9
kyletamayo <kyle.tamayo@gmail.com>
2015-04-23 09:03:24 -0500

    Having issues testing this hit here at school
    keep getting failure to connect to network host... I'll try it more at
    homes.

--- contact.aspx
+++ contact.aspx
@@ -29,7 +29,7 @@
         Text=""
         Visible="false">
     </asp:Label>
-
+    
     <br /><br />
 
     <asp:Button ID="btnCollect" 
--- contact.aspx.cs
+++ contact.aspx.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -13,40 +14,29 @@ public partial class Contact : System.Web.UI.Page
 
     }
 
-    protected void sendEmail()
-    {
-        var fromAddress = "lexipadmn@gmail.com";
-        var toAddress = "lexipadmn@gmail.com";
-        const String pass = "pixel7879";
-        string subj = "Comments";
-        string body = "From: " + tbName.Text + "\n Comments" + tbComments.Text;
-        var smtp = new System.Net.Mail.SmtpClient();
-        {
-            smtp.Host = "smtp.gmail.com";
-            smtp.Port = 847;
-            smtp.EnableSsl = true;
-            smtp.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
-            smtp.Credentials = new NetworkCredential(fromAddress, pass);
-            smtp.Timeout = 20000;
-        }
-
-        smtp.Send(fromAddress, toAddress, subj, body);
-
-    }
-
     protected void btnCollect_Click(object sender, EventArgs e)
     {
         try
         {
-            sendEmail();
-            lblMessage.Text = "Sent Successfully!";
-            lblMessage.Visible = true;
-            tbComments.Text = "";
+            MailMessage mail = messageCreation();
+            SmtpClient client = new SmtpClient();
+            client.Send(mail);
+            Response.Write("Mail Sent!");
         }
-        catch (Exception)
+        catch(Exception exception)
         {
-
+            lblMessage.Visible = true;
+            lblMessage.Text = exception.ToString();
         }
+    }
 
+    protected MailMessage messageCreation()
+    {
+        MailMessage mail = new MailMessage();
+        mail.To.Add("lexipadmn@gmail.com");
+        mail.From = new MailAddress("lexipadmn@gmail.com", tbName.Text);
+        mail.Subject = "Contact Us";
+        mail.Body = tbComments.Text;
+        return mail;
     }
 }
