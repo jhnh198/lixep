86da3d234e76f2ca86315f0d4bd1ace5f1af51ff
acbldie <antoniocbeldie@gmail.com>
2015-04-26 13:59:02 -0500

    Updated.

--- scoreboard.aspx.cs
+++ scoreboard.aspx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,4 +11,23 @@ public partial class Scoreboard : System.Web.UI.Page
     {
 
     }
+    protected void Button1_Click1(object sender, EventArgs e)
+    {
+        string url = Image1.ImageUrl;
+        Image1.ImageUrl = Image2.ImageUrl;
+        Image2.ImageUrl = Image3.ImageUrl;
+        Image3.ImageUrl = Image4.ImageUrl;
+        Image4.ImageUrl = url;
+
+    }
+    protected void Button2_Click1(object sender, EventArgs e)
+    {
+        string url = Image4.ImageUrl;
+        Image4.ImageUrl = Image3.ImageUrl;
+        Image3.ImageUrl = Image2.ImageUrl;
+        Image2.ImageUrl = Image1.ImageUrl;
+        Image1.ImageUrl = url;
+    }
+
+    
 }
