4f201c480b6a60cdff6340cc1d12078d534193b5
Antonio Monreal <monrealantonio@gmail.com>
2015-04-26 22:06:09 -0500

    Added Profile Functionality
    Added profile functionality which allows a user to add their name,
    location and a small about me section on the account page.

--- Secured_User_Pages/myAccount.aspx
+++ Secured_User_Pages/myAccount.aspx
@@ -4,22 +4,57 @@
     My Account
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="cntPageMain" runat="Server">
-    <div>
-        <p>My name: </p>
+    
+    <h1>Welcome 
+    <asp:Label ID="lblUserName" 
+        runat="server" 
+        Text="">
+    </asp:Label>
+    </h1>
+    <br /><br />
 
-        <asp:Label ID="lblUserName" 
-            runat="server" 
-            Text="">
-        </asp:Label>
-
-        <br />
-        <br />
-
+    <div id="initialInfo" runat="server" visible="false">
         <asp:Label ID="lblTimeLoggedIn"
             runat="server"
             Text="">
         </asp:Label>
+        <br /> <br />
 
+        <asp:Label ID="lblFirstName" runat="server" Text="First Name: "></asp:Label>
+        <asp:TextBox ID="txtFirstName" runat="server"></asp:TextBox> &nbsp;&nbsp;
+        <br />
+        <asp:Label ID="lblLastName" runat="server" Text="Last Name: "></asp:Label>
+        <asp:TextBox ID="txtLastName" runat="server"></asp:TextBox> &nbsp;&nbsp;
+        <br />
+        <asp:Label ID="lblLocation" runat="server" Text="Location: "></asp:Label>
+        <asp:TextBox ID="txtLocation" Text="" runat="server"></asp:TextBox> &nbsp;&nbsp;
+        <br />
+        <asp:Label ID="lblAbout" runat="server" Text="About Me: "></asp:Label>
+        <asp:TextBox ID="txtAbout" runat="server"></asp:TextBox>
+        <br />
+
+        <br /><br />
+        <asp:Button ID="btnSettings" OnClick="btnSettings_Click" runat="server" Text="Save Settings" /> &nbsp;&nbsp;
+        <asp:Button ID="btnCancel" OnClick="btnCancel_Click" runat="server" Text="Cancel Settings" /><br /><br />
+    </div>
+    <div id="savedInfo" runat="server" visible="false">
+
+        <asp:Label ID="lblFirstNameSaved" runat="server" Text="First Name: "></asp:Label>
+        <asp:Label ID="lblShowFirstName" runat="server" Text=""></asp:Label> <br /> <br />
+
+        <asp:Label ID="lblLastNameSaved" runat="server" Text="Last Name: "></asp:Label>
+        <asp:Label ID="lblShowLastName" runat="server" Text=""></asp:Label><br /><br />
+
+        <asp:Label ID="lblLocationSaved" runat="server" Text="Location: "></asp:Label>
+        <asp:Label ID="lblShowLocation" runat="server" Text=""></asp:Label><br /><br />
+
+        <asp:Label ID="lblAboutSaved" runat="server" Text="About Me: "></asp:Label>
+        <asp:Label ID="lblShowAbout" runat="server" Text=""></asp:Label><br /><br />
+
+        <asp:Button ID="btnEdit" OnClick="btnEdit_Click" runat="server" Text="Edit Settings" />
+    </div>
+    <div>
+        <br /><br />
         <asp:ChangePassword ID="ChangePassword1"
                 runat="server">
         </asp:ChangePassword>
--- Secured_User_Pages/myAccount.aspx.cs
+++ Secured_User_Pages/myAccount.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,16 +11,86 @@ public partial class Secured_User_Pages_myAccount : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if(!IsPostBack)
+        {
+            ProfileCommon profile = Profile.GetProfile(User.Identity.Name);
+            if(profile.LastUpdatedDate == DateTime.MinValue)
+            {
+                initialInfo.Visible = true;
+            }
+            else
+            {
+                savedInfo.Visible = true;
+                showProfile();
+            }
+
+            HttpCookie cookie = Request.Cookies["Preferences"];
+            if (cookie == null)
+            {
+
+            }
+        }
+
+
         if (System.Web.HttpContext.Current.User.Identity.IsAuthenticated)
         {
             lblUserName.Text = System.Web.HttpContext.Current.User.Identity.Name;
         }
+
         else lblUserName.Text = "user not logged in or invalid credentials.";
-        TimeSpan time = (((DateTime)Session["SessionStart"]).AddDays(7) - DateTime.Now);
-        int days = time.Days;
-        int hours = time.Hours;
-        int minutes = time.Minutes;
-        lblTimeLoggedIn.Text = "You have been logged in for " + days + " days " + hours + " hours " + minutes + " minutes.";
+
+        //TimeSpan time = (((DateTime)Session["SessionStart"]).AddDays(7) - DateTime.Now);
+        //int days = time.Days;
+        //int hours = time.Hours;
+        //int minutes = time.Minutes;
+        //lblTimeLoggedIn.Text = "You have been logged in for " + days + " days " + hours + " hours " + minutes + " minutes.";
     }
 
+    private void showProfile()
+    {
+        lblShowFirstName.Text = Profile.FirstName;
+        lblShowLastName.Text = Profile.LastName;
+        lblShowLocation.Text = Profile.Location;
+        lblShowAbout.Text = Profile.About;
+        savedInfo.Visible = true;
+    }
+
+    protected void btnSettings_Click(object sender, EventArgs e)
+    {
+        HttpCookie cookie = Request.Cookies["Preferences"];
+        if (cookie == null)
+        {
+            cookie = new HttpCookie("Preferences");
+        }
+
+        saveProfile();
+        showProfile();
+        initialInfo.Visible = false;
+    }
+
+    private void saveProfile()
+    {
+        Profile.FirstName = txtFirstName.Text;
+        Profile.LastName = txtLastName.Text;
+        Profile.Location = txtLocation.Text;
+        Profile.About = txtAbout.Text;
+        Profile.Save();
+    }
+
+
+    protected void btnCancel_Click(object sender, EventArgs e)
+    {
+        showProfile();
+        initialInfo.Visible = false;
+    }
+    protected void btnEdit_Click(object sender, EventArgs e)
+    {
+        initialInfo.Visible = true;
+        savedInfo.Visible = false;
+        txtFirstName.Text = Profile.FirstName;
+        txtLastName.Text = Profile.LastName;
+        txtLocation.Text = Profile.Location;
+        txtAbout.Text = Profile.About;
+        btnCancel.Visible = true;
+    }
 }
