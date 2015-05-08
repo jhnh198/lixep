4253497db95fcc28aabbd2ac1d333dc176bcfc98
John Hewitt <jchewitt@neiu.edu>
2015-05-07 19:22:53 -0500

    Revert "Deleted much unnecessary old pages"
    This reverts commit 416981b7b3f0ad1a680263519a238534f4683b7c.

---
+++ games.aspx.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.ServiceModel.Channels;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Windows;
+
+public partial class Games : System.Web.UI.Page
+{
+    //reference in making the gv command options
+    //https://msdn.microsoft.com/en-us/library/system.web.ui.webcontrols.buttonfield.commandname.aspx?cs-save-lang=1&cs-lang=csharp#code-snippet-2
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Application.Lock();
+        int pageHit = Convert.ToInt32(Application["PageHitsGames"]) + 1;
+        lblPageCount.Text = "You have visited this page " + pageHit.ToString() + " times.";
+        Application["PageHitsGames"] = pageHit;
+        Application.UnLock();
+    }
+
+    public float averageScore(float reviewScore, int numOfReviews){
+        return reviewScore / numOfReviews;
+    }
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnDownloadGame_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    //only a cosmetic rating. no single user rating only and no db update
+    protected void gv_games_RowCommand(Object sender, GridViewCommandEventArgs e)
+    {
+        //SqlConnection con = new SqlConnection(gvGames.DataSourceID);
+        
+        int index = Convert.ToInt32(e.CommandArgument);
+        GridViewRow selectedRow = gvGames.Rows[index];        
+
+        if (e.CommandName == "Download"){
+            TableCell gameFileCell = selectedRow.Cells[6];
+            string gameFileToDL = gameFileCell.Text;
+            string DLDir = "~/gameDir/pong.txt";
+
+            FileInfo file = new FileInfo(DLDir);
+            if (file.Exists)
+            {
+                Response.ContentType = "text/plain";
+                Response.AddHeader("Content-Disposition", "attachment; filename=" + DLDir);
+                Response.TransmitFile(Server.MapPath("~/gameDir/pong.txt"));
+                Response.End();
+            }
+        }
+        else if (e.CommandName == "Rate") {
+            GridViewRow i = gvGames.SelectedRow;
+
+            TableCell gameAverageCell = selectedRow.Cells[3];
+            TableCell gameTotalCell = selectedRow.Cells[4];
+            TableCell gameTotalRatingsCell = selectedRow.Cells[5];
+            double userRating = double.Parse(rblRating.SelectedValue);
+            double gameNewTotal = userRating + double.Parse(gameTotalCell.Text);
+            double gameNewTotalRatings = double.Parse(gameTotalRatingsCell.Text);
+            gameNewTotalRatings++;
+            double gameNewAverage = gameNewTotal / gameNewTotalRatings;
+            gameAverageCell.Text = gameNewAverage.ToString();
+            gameTotalCell.Text = gameNewTotal.ToString();
+            gameTotalRatingsCell.Text = gameNewTotalRatings.ToString();
+        }
+    }
+    protected void Page_PreInit(object sender, EventArgs e)
+    {
+        HttpCookie cookie = Request.Cookies["Prefrences"];
+        if (cookie != null)
+        {
+            Page.Theme = cookie["StyleSelection"];
+        }
+    }
+}
---
+++ relatedSites.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="RelatedSites.aspx.cs" Inherits="RelatedSites" Theme="Style" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+    Related Sites
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="cntPageMain" runat="server">
+    <asp:Label ID="lblPageCount"
+        runat="server"
+        Text=""/>
+    <br /><br />
+
+    <span>
+        <asp:ImageButton ID="ibtnTwitch" ImageUrl="~/Images/twitchLogo.png" runat="server" PostBackUrl="http://twitch.tv/" Width="200px" Height="200px"/>
+        <p>Twitch is a great site for watching some of the most popular games out today along with a few hidden classics. Check it out, watch the streamer, and chat with others all on one site!</p>
+    </span>
+    <br /><br />
+    <span>
+        <asp:ImageButton ID="ibtnYoutube" ImageUrl="~/Images/youtube.jpg" runat="server" PostBackUrl="http://www.youtube.com/" Width="400px" Height="200px"/>
+        <p>From 'Let's Plays' to tutorials for playing and even making your own games, this site has everything a gamer could ask for!</p>
+    </span>
+    <br /><br />
+    <span>
+        <asp:ImageButton ID="ibtnIGN" ImageUrl="~/Images/ign.jpg" runat="server" PostBackUrl="http://www.ign.com/" Width="200px" Height="200px"/>
+        <p>IGN is a great site on some of the most recent news about upcoming games, movies and major events in the gaming community.</p>
+    </span>
+</asp:Content>
+
